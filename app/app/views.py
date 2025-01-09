2ca0812c951cc62c55de92a2a25fca1e621549af
dylmcgarry <dylan.t.mcgarry@gmail.com>
2025-01-09 15:11:48 -0500

    fixed file structure by removing redundant app folder and linked home and about page to render their templates sotred in templates directory

--- app/app/views.py
+++
@@ -1,9 +0,0 @@
-from app import app
-
-@app.route("/")
-def index():
-    return "Hello world!"
-
-@app.route("/about")
-def about():
-    return "<h1 style='color: red'>About!!!!!!</h1>"
---
+++ app/views.py
@@ -0,0 +1,11 @@
+from app import app
+
+from flask import render_template
+
+@app.route("/")
+def index():
+    return render_template("/public/home-public.html")
+
+@app.route("/about")
+def about():
+    return render_template("/about.html")
